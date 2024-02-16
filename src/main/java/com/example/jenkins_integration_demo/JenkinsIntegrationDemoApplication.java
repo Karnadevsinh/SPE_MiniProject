e571b34a0affa5a43601811506ef2cd49e58d7f1
Karan <Karnadevsinh.Zala@iiitb.ac.in>
2024-02-16 17:20:36 +0530

    Initial setup

---
+++ src/main/java/com/example/jenkins_integration_demo/JenkinsIntegrationDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.jenkins_integration_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsIntegrationDemoApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+        SpringApplication.run(JenkinsIntegrationDemoApplication.class, args);
+    }
+
+}
